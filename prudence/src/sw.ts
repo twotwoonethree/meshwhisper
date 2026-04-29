a28ac34ecd2930d41af157c1b2031e52672af47b
Anton Mannering <anton@gestureloop.com>
2026-04-29 22:14:46 +0100

    fix: skipWaiting + clientsClaim so PWA updates activate immediately

--- prudence/src/sw.ts
+++ prudence/src/sw.ts
@@ -1,7 +1,11 @@
 /// <reference lib="webworker" />
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+import { clientsClaim } from 'workbox-core';
 
 declare const self: ServiceWorkerGlobalScope;
+
+self.skipWaiting();
+clientsClaim();
 
 precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
